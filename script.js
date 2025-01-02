625d5e1b056d29178c9846047076c0c7f36cf238
JyotiradeetiBagal <139766154+JyotiradeetiBagal@users.noreply.github.com>
2025-01-02 22:31:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	let sortedArr=arr.sort();
+	let minsum=0;
+	for(let i=0;i<sortedArr.length;i++){
+		let sum=0;
+		for(let j=0;j<=i;j++){
+			sum+=sortedArr[j];
+		}
+		minisum+=sum;
+	}
+	return minsum;
   
 }
 
